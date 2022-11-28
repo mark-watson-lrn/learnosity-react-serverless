4fbf7079a329f9f72b6c0dae4d25bcfa465ddb70
Mark Watson <mark.watson@learnosity.com>
2022-11-29 10:15:29 +1100

    added header to function.

--- netlify/functions/quiz-loader.js
+++ netlify/functions/quiz-loader.js
@@ -82,6 +82,10 @@ exports.handler = async function() {
 
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true'
+        },
         body: JSON.stringify({request})
     }
 }
