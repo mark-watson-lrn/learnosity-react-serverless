2fecc8e5bea300134fa1a5d43143486b074e7071
Mark Watson <mark.watson@learnosity.com>
2022-11-30 12:36:07 +1100

    trying to get the domain to authenticate

--- netlify/functions/reports-loader.js
+++ netlify/functions/reports-loader.js
@@ -6,7 +6,7 @@ exports.handler = async function(event) {
     // - - - - - - Learnosity's server-side configuration - - - - - - //
 
     // let domain = 'localhost';
-    let domain = '*';
+    let domain = 'learnosity-serverless.netlify.app';
 
     let assessUserRefererString = event.headers.referer;
     let userString = assessUserRefererString.substring(assessUserRefererString.indexOf('=') + 1);
