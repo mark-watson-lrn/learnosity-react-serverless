af6ddc63268046ef72baeef7ee8b14c4b7d0de77
Mark Watson <mark.watson@learnosity.com>
2023-06-30 10:33:04 +1000

    [POLISH]-tidy up and adding a video caption file

---
+++ src/assets/video-sub-en.vtt.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-type: text/vtt');
+?>
+WEBVTT FILE
+
+1
+00:00:00.500 --> 00:00:03.000 D:vertical A:start
+Everyone wants the <b><i>most from life</i></b>...
+
+2
+00:00:03.100 --> 00:00:05.000 A:start
+Like internet experiences that are rich <b>and</b> entertaining
+
+3
+00:00:05.200 --> 00:00:10.000 A:end
+Phone conversations where people truly <strong>connect</strong>
+
+4
+00:00:10.500 --> 00:00:15.000
+Your favourite TV programmes ready to watch at the touch of a button
+
+5
+00:00:15.500 --> 00:00:20.000
+Which is why we are bringing TV, internet and phone together in <strong>one</strong> super package
+
+6
+00:00:20.500 --> 00:00:24.000
+<b>One</b> simple way to get everything
+
+7
+00:00:24.500 --> 00:00:26.500 L:12%
+UPC
+
+8
+00:00:26.550 --> 00:00:30.000 L:75%
+Simply for <u>everyone</u>
